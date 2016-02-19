667a8dd40b4a996e219cbe188323c927b8bf1eca
Ed Fisher <ed@debacle.org>
2016-02-19 17:50:22 -0600

    modern rhel releases support sysctl.d

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,6 @@ default[:sysctl] = {}
 default[:sysctl][:values] = {}
 
 default[:sysctl][:config_file] = '/etc/sysctl.conf'
-if platform_family == 'debian'
+if platform_family == 'debian' || platform_family == 'rhel'
   default[:sysctl][:config_file] = '/etc/sysctl.d/99-chef.conf'
 end
